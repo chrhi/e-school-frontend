d7139af05fd0d8febf7023943fb59d9a1bc27f5b
abdellah <Abdellah.chehri14@gmail.com>
2024-12-18 07:17:50 +0100

    feat: update the layout

--- src/app/(marketing)/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function page() {
-  return <div>this is the landing page</div>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,31 @@
 import Providers from "@/components/providers";
-import "./styles/globals.css";
+import "@/styles/globals.css";
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { Toaster } from "@/components/ui/sonner";
-import React from "react";
+import type React from "react";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "300", "500", "700", "900"],
+	subsets: ["latin"],
+	weight: ["100", "300", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
-  title: "E-learnning",
-  description: "something we are trying to build it ",
+	title: "E-learnning",
+	description: "something we are trying to build it ",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <Providers>{children}</Providers>
-        <Toaster />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={poppins.className}>
+				<Providers>{children}</Providers>
+				<Toaster />
+			</body>
+		</html>
+	);
 }
