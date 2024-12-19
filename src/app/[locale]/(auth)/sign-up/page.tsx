d815178392f0d674c1141502e4924f2b3192ce1a
abdellah <Abdellah.chehri14@gmail.com>
2024-12-19 17:33:38 +0100

    feat: link two pages together

--- src/app/[locale]/(auth)/sign-up/page.tsx
+++ src/app/[locale]/(auth)/sign-up/page.tsx
@@ -1,25 +1,23 @@
 import AuthForm from "@/components/authForm";
 import Image from "next/image";
 
-import React from "react";
-
 function page() {
-	return (
-		<div className="flex gap-2 justify-around">
-			<div className="flex items-center justify-center h-screen">
-				<Image
-					src="/imageAuth1.svg"
-					alt="Auth Image"
-					width={600}
-					height={600}
-					className=""
-				/>
-			</div>
-			<div className="">
-				<AuthForm />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex gap-2 justify-around">
+      <div className="flex items-center justify-center h-screen">
+        <Image
+          src="/imageAuth1.svg"
+          alt="Auth Image"
+          width={600}
+          height={600}
+          className=""
+        />
+      </div>
+      <div className="">
+        <AuthForm />
+      </div>
+    </div>
+  );
 }
 
 export default page;
