cad3f8856882481d4bdaa954ae6c0220d994d98b
abdellah <Abdellah.chehri14@gmail.com>
2024-12-19 18:32:45 +0100

    feat: update the header

--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -26,8 +26,8 @@ export default function NavBar() {
 
   return (
     <div
-      className={`w-full h-[80px] z-[100] fixed top-0 transition-colors duration-300 ${
-        isScrolled ? "bg-white shadow-md" : "bg-transparent"
+      className={`w-full h-[80px] z-[100] fixed top-0 transition-colors duration-300 border-b ${
+        isScrolled ? "bg-white shadow-md " : " bg-white "
       }`}
     >
       <MaxWidthWrapper className="flex items-center justify-between w-full h-[80px]">
@@ -42,12 +42,12 @@ export default function NavBar() {
         </button>
 
         {/* Desktop Navigation */}
-        <div className="hidden md:flex w-full flex-grow items-center justify-end gap-x-8">
-          <div className="w-fit h-[70px] flex items-center justify-end gap-x-4">
+        <div className="hidden md:flex w-full flex-grow items-center justify-end gap-x-12">
+          <div className="w-fit h-[70px] flex items-center justify-end gap-x-8">
             {navs.map((item) => (
               <button
                 key={item}
-                className="hover:text-[#f46506] text-lg font-bold text-gray-900 transition-colors"
+                className="hover:text-[#f46506]  font-bold text-gray-900 transition-colors"
               >
                 {item}
               </button>
@@ -56,14 +56,14 @@ export default function NavBar() {
           <div className="w-fit flex items-center justify-end gap-x-2">
             <Link
               href={"/en/sign-up"}
-              className="px-4 py-2 text-lg  font-extrabold rounded-full bg-white border border-[#f46506] text-[#f46506] hover:bg-gray-100 transition-colors"
+              className="px-4 py-2  font-extrabold rounded-full bg-white border border-[#f46506] text-[#f46506] hover:bg-gray-100 transition-colors"
             >
               login
             </Link>
 
             <Link
               href={"/en/sign-up"}
-              className="px-4 py-2 text-lg bg-[#f46506]  font-extrabold rounded-full text-white hover:bg-[#e07e05] transition-colors"
+              className="px-4 py-2  bg-[#f46506]  font-extrabold rounded-full text-white hover:bg-[#e07e05] transition-colors"
             >
               register
             </Link>
