0895cbd370fc18b9ff9001d7ec7dfa65be4ac2df
abdellah <Abdellah.chehri14@gmail.com>
2024-12-20 06:51:17 +0100

    feat: add tidion chat

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,51 +5,56 @@ import { Poppins } from "next/font/google";
 import { notFound } from "next/navigation";
 
 const poppins = Poppins({
-	subsets: ["latin"],
-	weight: ["100", "300", "400", "500", "600"],
-	variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "600"],
+  variable: "--font-poppins",
 });
 
 export function generateStaticParams() {
-	return [{ locale: "en" }];
+  return [{ locale: "en" }];
 }
 
 async function getMessages(locale: string) {
-	try {
-		const messages = (await import(`../../i18n/words/${locale}.json`)).default;
-		return messages;
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	} catch (error) {
-		notFound();
-	}
+  try {
+    const messages = (await import(`../../i18n/words/${locale}.json`)).default;
+    return messages;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    notFound();
+  }
 }
 
 export default async function RootLayout({
-	children,
-	params,
+  children,
+  params,
 }: {
-	children: React.ReactNode;
-	params: Promise<{ locale: string }>;
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
-	const { locale } = await params;
+  const { locale } = await params;
 
-	if (locale !== "en") {
-		notFound();
-	}
+  if (locale !== "en") {
+    notFound();
+  }
 
-	setRequestLocale(locale);
+  setRequestLocale(locale);
 
-	const messages = await getMessages(locale);
+  const messages = await getMessages(locale);
 
-	const fontClass = poppins.className;
+  const fontClass = poppins.className;
 
-	return (
-		<html lang={locale}>
-			<body className={fontClass}>
-				<NextIntlClientProvider locale={locale} messages={messages}>
-					<div className="w-full  min-h-screen h-fit">{children}</div>
-				</NextIntlClientProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang={locale}>
+      <body className={fontClass}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <div className="w-full  min-h-screen h-fit">{children}</div>
+        </NextIntlClientProvider>
+      </body>
+
+      <script
+        src="//code.tidio.co/f4ntqdkrkcmkov2pbgapiuevig4fhtbq.js"
+        async
+      ></script>
+    </html>
+  );
 }
